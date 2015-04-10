52f98ae8605649675c5126f8a420913b373c86f8
Bruno Renié <brutasse@gmail.com>
2015-04-10 17:50:09 +0200

    Expand test matrix

--- runtests.py
+++ runtests.py
@@ -3,21 +3,28 @@ import os
 import sys
 import warnings
 
+import django
+
 warnings.simplefilter('always')
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'password_reset.tests.settings'
 
-from django.test.simple import DjangoTestSuiteRunner
+try:
+    from django.test.runner import DiscoverRunner
+except ImportError:
+    from discover_runner import DiscoverRunner
 
 
-def runtests(*test_args):
-    test_args = test_args or ['tests']
+def runtests():
     parent = os.path.dirname(os.path.abspath(__file__))
     sys.path.insert(0, parent)
 
-    runner = DjangoTestSuiteRunner(verbosity=1, interactive=True,
-                                   failfast=bool(os.environ.get('FAILFAST')))
-    failures = runner.run_tests(test_args)
+    if django.VERSION >= (1, 7):
+        django.setup()
+
+    runner = DiscoverRunner(verbosity=1, interactive=True,
+                            failfast=bool(os.environ.get('FAILFAST')))
+    failures = runner.run_tests(())
     sys.exit(failures)
 
 
