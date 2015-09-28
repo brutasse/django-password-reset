53ea6952df658fb50dc125c0e7f4449cde59021d
Bruno Renié <brutasse@gmail.com>
2015-09-28 14:26:19 +0200

    Update code for django 1.8

--- password_reset/urls.py
+++ password_reset/urls.py
@@ -1,14 +1,13 @@
-from django.conf.urls import url, patterns
+from django.conf.urls import url
 
 from . import views
 
 
-urlpatterns = patterns(
-    '',
+urlpatterns = [
     url(r'^recover/(?P<signature>.+)/$', views.recover_done,
         name='password_reset_sent'),
     url(r'^recover/$', views.recover, name='password_reset_recover'),
     url(r'^reset/done/$', views.reset_done, name='password_reset_done'),
     url(r'^reset/(?P<token>[\w:-]+)/$', views.reset,
         name='password_reset_reset'),
-)
+]
