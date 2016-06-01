6f76ed4045edc1bfd591215f172de920112963e0
Ross Crawford-d'Heureuse <sendrossemail@gmail.com>
2016-06-01 13:52:41 +0200

    allowed override from calling app

--- password_reset/__init__.py
+++ password_reset/__init__.py
@@ -1,1 +1,21 @@
+from django.conf import settings
+
+try:
+    from django.contrib.sites.shortcuts import get_current_site
+except ImportError:
+    from django.contrib.sites.models import get_current_site
+
 __version__ = '0.8.1'
+
+DEFAULT_SETTINGS = {
+		'token_expires': 3600 * 48,  # Two days
+		'get_current_site': get_current_site
+}
+
+PASSWORD_RESET_SETTINGS = getattr(settings,
+																	'PASSWORD_RESET_SETTINGS',
+																	DEFAULT_SETTINGS)
+
+
+DEFAULT_SETTINGS.update(PASSWORD_RESET_SETTINGS)
+print PASSWORD_RESET_SETTINGS
